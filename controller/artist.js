cdbaa54477d9d742185cbaadf764950647ce6581
mfonseca <fonsecamatiasm@gmail.com>
2020-01-28 12:10:37 -0300

    agrego update y list del artista

--- controller/artist.js
+++ controller/artist.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var Artist = require('../model/artist');
 var Album = require('../model/album');
 var Song = require('../model/song');
+var mongoosePagination = require('mongoose-pagination');
 
 function getArtist(req,res){
     var artistId = req.params.id;
@@ -21,6 +22,50 @@ function getArtist(req,res){
             }
         }
     })
+}
+
+function getArtists(req,res){
+    if(req.params.page){
+        var page = req.params.page;    
+    }else{
+        var page = 1; //valor default
+    }
+    
+    var itemsperpage = 3;
+
+    Artist.find().sort('name').paginate(page, itemsperpage, function(err,artists,total){
+        if(err){
+            res.status(500).send({message:"Error en la petision"});
+        }else{
+            if(!artists){
+                res.status(404).send({message:"No hay artistas"});
+            }else{
+                return res.status(200).send({
+                    total_items:total,
+                    artists: artists
+                });
+            }
+        }
+    } );
+
+}
+function updateArtist(req,res){
+    var artistId = req.params.id;
+    var update = req.body;
+
+    Artist.findByIdAndUpdate(artistId, update, (err,artistUpdate) => {
+        if(err){
+            res.status(500).send({message:"Error en la petision"});
+        }else{
+            if(!artistUpdate){
+                res.status(404).send({message:"El artista no ha sido actualizado"});
+            }else{
+                res.status(200).send({
+                artist:artistUpdate    
+                });
+            }
+        }
+    });
 }
 
 function saveArtist(req,res){
@@ -47,5 +92,7 @@ function saveArtist(req,res){
 
 module.exports = {
     getArtist,
-    saveArtist
+    saveArtist,
+    getArtists,
+    updateArtist,
 }
